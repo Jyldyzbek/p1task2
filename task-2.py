8166b993ddf519a630486c5a52da1bf0df600d93
jikish <jikish2103dk@gmail.com>
2019-11-12 21:14:32 +0600

    Part-1 Task-2

---
+++ task-2.py
@@ -0,0 +1,6 @@
+def square_number(x): x ** 2
+def test_square_number():
+    assert square_number(2) == 4
+    assert square_number(8) == 64
+    assert square_number(10) == 100
+print("YOUR CODE IS CORRECT!")
